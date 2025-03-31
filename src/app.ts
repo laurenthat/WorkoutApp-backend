66a44c391c1595033699e694780d5fe16946e12e
laurenthat <116711536+laurenthat@users.noreply.github.com>
2025-03-31 22:22:25 +0300

    first commit of the backend

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// import workoutRoutes from './routes/workoutRoutes';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+// app.use('/api', workoutRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
